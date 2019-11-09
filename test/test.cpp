ba67981c9181ddc9dee34a159267bf96383b8ea0
Andrey Filipenkov <decapitator@ukr.net>
2019-11-09 16:17:51 +0300

    add test files to the repo, create link to their dir

--- test/test.cpp
+++ test/test.cpp
@@ -6,10 +6,13 @@
 
 TEST_CASE("constructors", "[tbl]")
 {
+    auto testFilePath = [](const std::string& fileName){
+        return fs::path{"data"} / fileName;
+    };
     CHECK_NOTHROW(Tbl{});
-    CHECK_THROWS_AS(Tbl{"foo.tbl"}, NoFileException);
-    CHECK_THROWS_AS(Tbl{EMPTY_FILE_NAME}, TblReadException);
-    CHECK_THROWS_MATCHES(Tbl{"wrong_size.tbl"}, TblTruncatedException, Catch::Predicate<TblTruncatedException>(
+    CHECK_THROWS_AS(Tbl{testFilePath("foo.tbl")}, NoFileException);
+    CHECK_THROWS_AS(Tbl{testFilePath("empty.tbl")}, TblReadException);
+    CHECK_THROWS_MATCHES(Tbl{testFilePath("wrong_size.tbl")}, TblTruncatedException, Catch::Predicate<TblTruncatedException>(
                              [](const TblTruncatedException& e) {
         return e.expectedFileSize == 77181 && e.actualFileSize == 22; }
     ));
--- test/test.pro
+++ test/test.pro
@@ -10,3 +10,15 @@ include(../3rdparty/catch/catch.pri)
 include(../3rdparty/filesystem/filesystem.pri)
 include(../tbl/tbl.pri)
 
+
+testDataDirName = data
+testDataInDir = $$shell_quote($$shell_path($${PWD}/$${testDataDirName}))
+testDataOutDir = $$shell_quote($$shell_path($${OUT_PWD}/$${testDataDirName}))
+
+win32:isEmpty(MINGW_IN_SHELL) {
+    QMAKE_POST_LINK += mklink /D $$testDataOutDir $$testDataInDir
+    QMAKE_POST_LINK += & if %errorlevel%==9009 echo "mklink doesn't exist"
+}
+else {
+    QMAKE_POST_LINK += [ -L $$testDataOutDir ] || ln -s $$testDataInDir $$testDataOutDir
+}
