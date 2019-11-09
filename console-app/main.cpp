a159282a3542ff645bcf09d6752cf946fe2df992
Andrey Filipenkov <decapitator@ukr.net>
2019-11-09 21:13:05 +0300

    implement simple args parsing

--- console-app/main.cpp
+++ console-app/main.cpp
@@ -2,20 +2,47 @@
 #include "exceptions.h"
 
 #include <iostream>
+#include <args.hxx>
 
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    if (argc < 2)
+    args::ArgumentParser parser("CLI for Diablo 2 string files.");
+    [[maybe_unused]] args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+    args::PositionalList<fs::path> filePaths(parser, "files", "Input files");
+    try
     {
-        cerr << "tbl param missing" << endl;
+        parser.ParseCLI(argc, argv);
+    }
+    catch (const args::Help&)
+    {
+        cout << parser;
+        return 0;
+    }
+    catch (const args::ParseError& e)
+    {
+        cerr << e.what() << endl;
+        cerr << parser;
         return 1;
+    }
+    catch (const args::ValidationError& e)
+    {
+        cerr << e.what() << endl;
+        cerr << parser;
+        return 1;
+    }
+
+    auto files = args::get(filePaths);
+    if (files.empty())
+    {
+        parser.Help(cout);
+        return 0;
     }
 
     try
     {
-        Tbl t{fs::path{argv[1]}};
+        Tbl t{files.at(0)};
         cout << "Hello World! " << &t << endl;
     }
     catch (const NoFileException& e)
