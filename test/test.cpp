41fa6bdc9adac95daba174d45e9bbb88e65d9f0e
Andrey Filipenkov <decapitator@ukr.net>
2019-11-13 20:51:38 +0300

    add tests for colors

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,11 @@
 
 #include "tbl.h"
 #include "exceptions.h"
+#include "d2color.h"
+
+#include <codecvt>
+
+using namespace std::string_literals;
 
 TEST_CASE("constructors", "[tbl]")
 {
@@ -18,4 +23,28 @@ TEST_CASE("constructors", "[tbl]")
                              [](const TblTruncatedException& e) {
         return e.expectedFileSize == 77181 && e.actualFileSize == 22; }
     ));
+}
+
+TEST_CASE("colors", "[D2Color]")
+{
+    using namespace D2Color;
+
+    CHECK(header == std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}.to_bytes(u"\u00FF\u0063"));
+    CHECK(header == u8"ÿc");
+
+    CHECK(readableColors.size() == Purple - White + 1);
+
+    CHECK(readableColor(Red).second == R"(\red;)");
+    CHECK_THROWS_AS(readableColor(White - 1), std::out_of_range);
+    CHECK_THROWS_AS(readableColor(Purple + 1), std::out_of_range);
+
+    auto rawColor = [](ColorCode code){ return header + static_cast<char>(code); };
+
+    auto s = rawColor(White) + "test" + rawColor(Red) + "qwe";
+    convertToReadableColors(s);
+    CHECK(s == readableColor(White).second + "test" + readableColor(Red).second + "qwe");
+
+    s = "asd" + header + "qwe" + rawColor(Blue);
+    convertToReadableColors(s);
+    CHECK(s == "asd" + header + "qwe" + readableColor(Blue).second);
 }
