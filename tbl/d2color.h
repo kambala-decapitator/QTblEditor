07047b45b84a584c5f91b00c4bae07eb82fcc432
Andrey Filipenkov <decapitator@ukr.net>
2020-08-16 21:34:56 +0300

    fix MSVC build

--- tbl/d2color.h
+++ tbl/d2color.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "common/tbl.h"
+
 namespace D2Color
 {
 enum ColorCode : unsigned char
@@ -19,9 +21,9 @@ enum ColorCode : unsigned char
     Purple,
 };
 
-extern const std::string header;
-extern const std::vector<std::pair<ColorCode, std::string>> readableColors;
+extern const std::string TBL_EXPORT header;
+extern const std::vector<std::pair<ColorCode, std::string>> TBL_EXPORT readableColors;
 
-extern decltype(readableColors)::value_type readableColor(char colorCode);
-extern void convertToReadableColors(std::string& s) noexcept;
+extern decltype(readableColors)::value_type TBL_EXPORT readableColor(char colorCode);
+extern void TBL_EXPORT convertToReadableColors(std::string& s) noexcept;
 }
